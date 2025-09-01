821b416307ba56907d75ddd635a094569bcb381f
raj <rajthakurrjtr7906674136@gmail.com>
2025-09-01 16:05:49 +0530

    first commit

---
+++ StudentRegistrationSystem.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class StudentRegistrationSystem {
+    public static void main(String[] args){
+        try(Scanner sc = new Scanner(System.in)){
+            int x = sc.nextInt();
+            for(int i=1;i==x;i++){
+                System.out.println("enter the name :");
+                String name = sc.nextLine(); 
+                String[] arr1 =new String[name.length()];
+            }
+            if (arr1[1] == raj){
+                System.out.println("Student registered");
+            }else{
+                System.out.println("Student not registered");
+            }
+             
+        }    
+    }      
+ } 
